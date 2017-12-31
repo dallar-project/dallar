88ea042a9a3267d8415c4b8c8ce0e86c8d70092b
Michael Allar <allar@michaelallar.com>
2017-12-31 09:11:55 -0800

    Removed test dependencies

--- src/serialize.h
+++ src/serialize.h
@@ -6,6 +6,7 @@
 #ifndef BITCOIN_SERIALIZE_H
 #define BITCOIN_SERIALIZE_H
 
+#include "clientversion.h"
 #include "compat/endian.h"
 
 #include <algorithm>
@@ -209,8 +210,8 @@ template<typename Stream> inline void Unserialize(Stream& s, bool& a) { char f=s
 inline unsigned int GetSizeOfCompactSize(uint64_t nSize)
 {
 	if (nSize < 253)             return sizeof(unsigned char);
-	else if (nSize <= USHRT_MAX) return sizeof(unsigned char) + sizeof(unsigned short);
-	else if (nSize <= UINT_MAX)  return sizeof(unsigned char) + sizeof(unsigned int);
+	else if (nSize <= std::numeric_limits<unsigned short>::max()) return sizeof(unsigned char) + sizeof(unsigned short); 
+    else if (nSize <= std::numeric_limits<unsigned int>::max())  return sizeof(unsigned char) + sizeof(unsigned int);
 	else                         return sizeof(unsigned char) + sizeof(uint64_t);
 }
 
